3858a2cccc4f02929575ebe4b554818e5ea11879
taku-271 <ta.yamashita.314@gmail.com>
2023-12-27 11:11:46 +0900

    feat: 準備中表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
+import { Box, Heading } from "@yamada-ui/react";
+
 const Index = () => {
-  return <h1>準備中です</h1>;
+  return (
+    <Box textAlign="center" my="xl">
+      <Heading>現在準備中です...</Heading>
+    </Box>
+  );
 };
 
 export default Index;
