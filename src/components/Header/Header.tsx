58e1c0d57e730764e5cb1bddd086355f3ef103f0
taku-271 <ta.yamashita.314@gmail.com>
2023-12-27 10:57:50 +0900

    refactor: リファクタ

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { tabs } from "@/constants";
 import { Box, Button, Heading } from "@yamada-ui/react";
 import { useRouter } from "next/router";
 
@@ -28,34 +29,16 @@ const Header = () => {
         alignItems="end"
         gap="30px"
       >
-        <Button
-          variant="link"
-          color="white"
-          onClick={() => onLinkClickHandler("")}
-        >
-          HOME
-        </Button>
-        <Button
-          variant="link"
-          color="white"
-          onClick={() => onLinkClickHandler("about")}
-        >
-          ABOUT
-        </Button>
-        <Button
-          variant="link"
-          color="white"
-          onClick={() => onLinkClickHandler("works")}
-        >
-          WORKS
-        </Button>
-        <Button
-          variant="link"
-          color="white"
-          onClick={() => onLinkClickHandler("link")}
-        >
-          LINK
-        </Button>
+        {tabs.map((tab) => (
+          <Button
+            variant="link"
+            color="white"
+            onClick={() => onLinkClickHandler(tab.link)}
+            key={tab.id}
+          >
+            {tab.name}
+          </Button>
+        ))}
       </Box>
     </Box>
   );
